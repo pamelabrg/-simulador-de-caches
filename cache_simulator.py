fd49fd17e0825d4395590a40e458ecbbca593ed8
Pâmela <pamelabraga.dossantos@gmail.com>
2025-03-11 21:38:34 -0300

    final

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,11 +1,10 @@
 from memoria_cache import MemoriaCache
 
 cache = MemoriaCache()
-#cache.imprimir_cache()
 
 with open(cache.arquivo_entrada, "rb") as f:
     while True:
-        prox_byte = f.read(4)  # Lendo 4 bytes
+        prox_byte = f.read(4)
         if not prox_byte:
             break  
         endereco = int.from_bytes(prox_byte, byteorder="big")
